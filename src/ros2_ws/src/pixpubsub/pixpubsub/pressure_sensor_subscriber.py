da1b14037ba9fee5576fdda4207d41c4e7161e40
ccs87 <dydhhdydyud@gmail.com>
2024-03-06 22:08:24 +0800

    Modify the subscriber code

--- src/ros2_ws/src/pixpubsub/pixpubsub/pressure_sensor_subscriber.py
+++ src/ros2_ws/src/pixpubsub/pixpubsub/pressure_sensor_subscriber.py
@@ -22,30 +22,29 @@ class MinimalSubscriber(Node):
         self.declare_parameter("gravity", 9.81) # m/s²
         self.declare_parameter("max_depth", 2) # meters
 
+    def calculate_depth(self, fluid_pressure):
+        # Get the configurable parameters
+        surface_pressure = self.get_parameter("surface_pressure").value
+        fluid_density = self.get_parameter("fluid_density").value
+        gravity = self.get_parameter("gravity").value
+        max_depth = self.get_parameter("max_depth").value
+
+        # Calculate the depth
+        depth = (fluid_pressure - surface_pressure) / (fluid_density * gravity)
+        # Ensure the depth is within the pool's maximum depth
+        if depth > max_depth:
+            depth = max_depth
+        return depth
+
     def listener_callback(self, msg):
         # Check if the 'fluid_pressure' field exists in the message
         if hasattr(msg, 'fluid_pressure'):
-
-            # Get the configurable parameters
-            surface_pressure = self.get_parameter("surface_pressure").value
-            fluid_density = self.get_parameter("fluid_density").value
-            gravity = self.get_parameter("gravity").value
-            max_depth = self.get_parameter("max_depth").value
-
-            # Calculate the depth
-            depth = (msg.fluid_pressure - surface_pressure) / (fluid_density * gravity)
-            # depth = (msg.fluid_pressure) / (fluid_density * gravity)
-
-            # Ensure the depth is within the pool's maximum depth
-            if depth > max_depth:
-                depth = max_depth
+            depth = self.calculate_depth(msg.fluid_pressure)
             self.get_logger().info(f'Calculated Depth: {depth} meters')
-
         else:
             self.get_logger().warn('The message does not contain a fluid_pressure field')
 
 def main(args=None):
-
     rclpy.init(args=args)
     minimal_subscriber = MinimalSubscriber()
     rclpy.spin(minimal_subscriber)
