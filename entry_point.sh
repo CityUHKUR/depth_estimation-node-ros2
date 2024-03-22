681567d89196b2a0fb2230d2798628c1f5a7d350
ur <dydhhdydyud@gmail.com>
2024-03-22 21:37:51 +0800

    Modify entry point

--- entry_point.sh
+++ entry_point.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
-
-source /opt/ros/humble/setup.bash
-echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
-
-if [ -f "/src/ros2_ws/install/setup.bash" ]; then
-    echo "source /src/ros2_ws/install/setup.bash" >> ~/.bashrc
-else
-    source /opt/ros/humble/setup.bash
-    cd /src/ros2_ws/
-    colcon build
-    echo "source /src/ros2_ws/install/setup.bash" >> ~/.bashrc
-fi
-
+echo "source /opt/ros/humble/local_setup.bash" >> ~/.bashrc
+echo "source /src/ros2_ws/install/setup.bash" >> ~/.bashrc
+# ros2 run imu_publisher publisher
+source /opt/ros/humble/local_setup.bash
 source /src/ros2_ws/install/setup.bash
-
-echo "alias bws='/scripts/build_ws.sh'" >> ~/.bashrc
-echo "alias sws='/scripts/source_ws.sh'" >> ~/.bashrc
-echo "Setup finished."
+# ros2 run pixpubsub pixhawk_worker
+colcon build 
 ros2 run depthsub Depth_node
+
 /bin/bash
---
+++ src/ros2_ws/build/depthsub/build/lib/depthsub/pressure_sensor_subscriber.py
@@ -0,0 +1,58 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import FluidPressure
+
+class MinimalSubscriber(Node):
+    def __init__(self):
+        super().__init__('minimal_subscriber')
+        self.get_logger().info("Starting depth node")
+        self.subscription = self.create_subscription(
+            FluidPressure,
+            'sensor_data',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+        # Initialize configurable parameters
+        self.initialize_parameters()
+
+    def initialize_parameters(self):
+        # Configurable parameters
+        self.declare_parameter("surface_pressure", 101325)  # Pa
+        self.declare_parameter("fluid_density", 1025)  # kg/m³
+        self.declare_parameter("gravity", 9.81)  # m/s²
+        self.declare_parameter("max_depth", 2)  # meters
+
+    def calculate_depth(self, fluid_pressure):
+        # Get the configurable parameters
+        surface_pressure = self.get_parameter("surface_pressure").value
+        fluid_density = self.get_parameter("fluid_density").value
+        gravity = self.get_parameter("gravity").value
+        max_depth = self.get_parameter("max_depth").value
+        # Calculate the depth
+        depth = (fluid_pressure - surface_pressure) / (fluid_density * gravity)
+        # Ensure the depth is within the pool's maximum depth
+        if depth > max_depth:
+            depth = max_depth
+        return depth
+
+    def listener_callback(self, msg):
+        self.get_logger().info("I heard msg {}".format(msg))
+        # Check if the 'fluid_pressure' field exists in the message
+        try:
+            if hasattr(msg, 'fluid_pressure'):
+                depth = self.calculate_depth(msg.fluid_pressure)
+                self.get_logger().info(f'Calculated Depth: {depth} meters')
+            else:
+                self.get_logger().info('The message does not contain a fluid_pressure field')
+        except:
+            self.get_logger().warn("fuck")
+            pass
+def main(args=None):
+    rclpy.init(args=args)
+    minimal_subscriber = MinimalSubscriber()
+    rclpy.spin(minimal_subscriber)
+    minimal_subscriber.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
